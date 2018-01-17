3004f7dbffba1151675462432184361286b95de1
yangxvhao <yangxuhaoss@gmail.com>
2018-01-17 18:33:11 +0800

    builder

---
+++ design-patterns/src/main/java/com/yangxvhao/demo/builderpatterns/AbstractMode.java
@@ -0,0 +1,10 @@
+package com.yangxvhao.demo.builderpatterns;
+
+/**
+ * @author yangxvhao
+ * @date 18-1-17.
+ */
+
+public abstract class AbstractMode {
+
+}
---
+++ design-patterns/src/main/java/com/yangxvhao/demo/builderpatterns/Exterior.java
@@ -0,0 +1,45 @@
+package com.yangxvhao.demo.builderpatterns;
+
+/**
+ * @author yangxvhao
+ * @date 18-1-17.
+ */
+
+public class Exterior {
+    private String menu;
+    private String playList;
+    private String mainWindow;
+    private String controlBar;
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public String getPlayList() {
+        return playList;
+    }
+
+    public void setPlayList(String playList) {
+        this.playList = playList;
+    }
+
+    public String getMainWindow() {
+        return mainWindow;
+    }
+
+    public void setMainWindow(String mainWindow) {
+        this.mainWindow = mainWindow;
+    }
+
+    public String getControlBar() {
+        return controlBar;
+    }
+
+    public void setControlBar(String controlBar) {
+        this.controlBar = controlBar;
+    }
+}

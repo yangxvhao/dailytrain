2927af365c958099805761d55d9ab85f1ac34260
yangxvhao <yangxuhaoss@gmail.com>
2018-01-22 15:57:29 +0800

    adapter pattern

--- design-patterns/src/main/java/com/yangxvhao/demo/structuralpattern/adapterpattern/scoreoperation/BinarySearch.java
+++ design-patterns/src/main/java/com/yangxvhao/demo/structuralpattern/adapterpattern/scoreoperation/BinarySearch.java
@@ -7,9 +7,27 @@ package com.yangxvhao.demo.structuralpattern.adapterpattern.scoreoperation;
  */
 
 public class BinarySearch {
+    /**
+     * 二分查找，基于有序数组
+     * @param nums
+     * @param num
+     * @return
+     */
     public int binarySearch(int[] nums, int num){
-        System.out.println("二分查找...");
-
-        return 1;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end){
+            int mid = (start + end)/2;
+            int midVal = nums[mid];
+            if(midVal < num)
+            {
+                start = mid + 1;
+            }else if(midVal > num){
+                end = mid - 1;
+            }else if(midVal == num){
+                return 1;
+            }
+        }
+        return -1;
     }
 }

022fd49af806c3df1ec035f8d761ad4a2fabaea9
yangxuhao <xuhao.yang@finlink.net.cn>
2018-07-24 15:15:38 +0800

    topic4

--- leetcode/src/main/java/com/yangxvhao/demo/Topic3.java
+++ leetcode/src/main/java/com/yangxvhao/demo/Topic3.java
@@ -2,10 +2,8 @@ package com.yangxvhao.demo;
 
 import org.springframework.util.StopWatch;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.lang.reflect.AccessibleObject;
+import java.util.*;
 
 /**
  * 给定一个字符串，找出不含有重复字符的最长子串的长度。
@@ -25,35 +23,64 @@ public class Topic3 {
     public static void main(String[] args) {
         StopWatch start = new StopWatch();
         start.start();
-        System.out.println(lengthOfLongestSubstring("puuywsnezdufctcjqmrkiwhwerepqyehsyirqvxryrwbmbmepfpzeyvkrzajzesteakwvhnwalznmnrbu"));
+        System.out.println(lengthOfLongestSubstring("ababc"));
         start.stop();
         System.out.println(start.getLastTaskTimeMillis());
         ;
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        if (s.isEmpty()) {
-            return 0;
+        int sum = 0;
+        int [] ascii = new int[128];
+        int n = s.length();
+        for(int i = 0,j = 0; j < n; j ++){
+            //跳过重复字符的位置
+            i = Math.max(i, ascii[s.charAt(j)]);
+            //比较现在窗口和已保存的大小
+            sum = Math.max(sum, j - i + 1);
+            //记录每个字符的最后索引
+            ascii[s.charAt(j)] = j + 1;
         }
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < s.toCharArray().length; ++i) {
-            String sumStr = String.valueOf(s.charAt(i));
-            list.add(String.valueOf(s.charAt(i)));
-            for (int j = i + 1; j < s.toCharArray().length; ++j) {
-                sumStr += String.valueOf(s.charAt(j));
-                list.add(sumStr);
-            }
-        }
-        Iterator<String> iterator = list.iterator();
-        List<Integer> count = new ArrayList<>();
-        while (iterator.hasNext()) {
-            String s1 = iterator.next();
-            if (!isRepeat(s1)) {
-                count.add(s1.length());
-            }
-        }
-        Collections.sort(count);
-        return count.get(count.size() - 1);
+        return sum;
+        
+//        int sum = 0, i = 0, j = 0;
+//        int n = s.length();
+//        HashSet<Character> set = new HashSet<>();
+//        while (i < n && j < n){
+//            if(!set.contains(s.charAt(j))){
+//                set.add(s.charAt(j++));
+//                sum = Math.max(sum, j-i);
+//            }else {
+//                set.remove(s.charAt(i++));
+//            }
+//        }
+//        return sum;
+        
+//        if (s.isEmpty()) {
+//            return 0;
+//        }
+//        List<String> list = new ArrayList<>();
+//        //1.找出s的所有子字符串
+//        for (int i = 0; i < s.toCharArray().length; ++i) {
+//            String sumStr = String.valueOf(s.charAt(i));
+//            list.add(String.valueOf(s.charAt(i)));
+//            for (int j = i + 1; j < s.toCharArray().length; ++j) {
+//                sumStr += String.valueOf(s.charAt(j));
+//                list.add(sumStr);
+//            }
+//        }
+//        //2.去掉所有含有重复字符的子字符串
+//        Iterator<String> iterator = list.iterator();
+//        List<Integer> count = new ArrayList<>();
+//        while (iterator.hasNext()) {
+//            String s1 = iterator.next();
+//            if (!isRepeat(s1)) {
+//                count.add(s1.length());
+//            }
+//        }
+//        //３.排序，找出最长的
+//        Collections.sort(count);
+//        return count.get(count.size() - 1);
     }
 
     private static boolean isRepeat(String s) {
---
+++ leetcode/src/main/java/com/yangxvhao/demo/Topic4.java
@@ -0,0 +1,24 @@
+package com.yangxvhao.demo;
+
+/**
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+ *
+ * 请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+ * 
+ * @author yangxvhao
+ * @date 2018-07-24 下午2:40.
+ */
+public class Topic4 {
+    public static void main(String[] args) {
+//        int [] nums1 = {1,3}, nums2 = {2};
+        int [] nums1 = {1,2}, nums2 = {3, 4};
+        findMedianSortedArrays(nums1, nums2);
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double average = 0;
+        int n1 = nums1.length, n2 = nums2.length;
+        
+        return average;
+    }
+}

5fa0e477d89cb58a77016ece7a4660ce99a0e329
yangxuhao <xuhao.yang@finlink.net.cn>
2018-08-08 13:46:22 +0800

    stream.of

--- stream/src/main/java/com/yangxvhao/demo/MainApp.java
+++ stream/src/main/java/com/yangxvhao/demo/MainApp.java
@@ -1,6 +1,10 @@
 package com.yangxvhao.demo;
 
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * A Camel Application
  */
@@ -10,7 +14,20 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-        System.out.println("hello world");
+//        String [] strings1 = new String[]{"aaa", "sss", "sdjgji"};
+//        String [] strings2 = new String[]{"bbb", "dddd", "ddfdfda"};
+//
+//        List<String> list1 = Arrays.asList(strings1);
+//        List<String> list2 = Arrays.asList(strings2);
+//        
+//        List<String> result = Stream.of(list1, list2).flatMap(Collection::stream).collect(Collectors.toList());
+//        
+        Map<String, String> map = new HashMap<>();
+        map.put("111", "aaa");
+        map.put(null, null);
+        
+        List list = new ArrayList(map.entrySet());
+        System.out.println();
     }
 
 }

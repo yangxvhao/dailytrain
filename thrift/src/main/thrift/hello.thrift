d2ff21e5e83ed95f8a53d517e15c2c15ed279dfe
yangxvhao <yangxvhao01@163.com>
2020-02-25 17:21:58 +0800

    thrift test

--- thrift/src/main/thrift/hello.thrift
+++ thrift/src/main/thrift/hello.thrift
@@ -1,3 +1,10 @@
-service HelloWorldDemo{
-    void hello(string msg)
+service HelloWorldService{
+    void hello(1: string msg);
+    string modify(1: HelloWorldRequest request);
+}
+
+struct HelloWorldRequest{
+    1: required string name;
+    2: optional i32 age;
+    3: bool archive;
 }
